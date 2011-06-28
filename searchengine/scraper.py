6ad9efad79d1c4f971e7915c816f647cd3ef341d
Jonathan Bydendyk <jpbydendyk@gmail.com>
2011-06-28 13:05:38 +0200

    completed simple html scraper and added tests

---
+++ searchengine/scraper.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+from HTMLParser import HTMLParser
+import optparse
+import urllib2
+
+class TagSelector(HTMLParser):
+    """
+    Strip all HTML tags from a string.
+    """
+    def __init__(self, *args, **kwargs):
+        self.reset()
+        self.content_dict = {
+            'title': "",
+            'content': [],
+            'links': [],
+        }
+        self.current_tag = None
+        self.current_attrs = None
+        self.visible_tags = kwargs.get('visible_tags', [
+            'body', 'title', 'p', 'div', 'td',
+            'span', 'blockquote', 'li', 'a',
+        ])
+        
+    def attr_dict(self, attrs):
+        """
+        Iterate through the attrs and convert to a dict.
+        """
+        attrs_dict = {}
+        if attrs:
+            for a in attrs:
+                attrs_dict.update({a[0]: a[1]})
+        return attrs_dict
+        
+    def handle_starttag(self, tag, attrs):
+        """
+        Set the current tag the parser has reached.
+        TODO: add css support
+        """
+        self.current_tag = tag
+        self.current_attrs = self.attr_dict(attrs)
+    
+    def handle_data(self, data):
+        """
+        Append visible data to the list.
+        """
+        data = data.strip()
+        
+        # find visible data and update to the content_dict
+        if self.current_tag in self.visible_tags and data:
+            if self.current_tag == 'title':
+                self.content_dict['title'] = data
+            if self.current_tag == 'a' and self.current_attrs.has_key('href'):
+                link = self.current_attrs['href']
+                self.content_dict['links'].append((data, link))
+            self.content_dict['content'].append(data)
+    
+    def get_data(self):
+        """
+        Join list and return as a string.
+        """
+        return self.content_dict
+
+class HTMLScraper(object):
+    """
+    A simple HTML Scraper class to get visable text from a given URL.
+    """
+    def __init__(self, url, *args, **kwargs):
+        self.url = url
+        
+    def get_url_content(self):
+        """
+        Open and read the content of a URL.
+        """
+        url_handler = urllib2.urlopen(self.url)
+        html_content = url_handler.read()
+        url_handler.close()
+        
+        return html_content
+        
+    def get_content(self):
+        """
+        Parse the visable content of a url into plain text.
+        """
+        html_content = self.get_url_content()
+        html_parser = TagSelector()
+        html_parser.feed(html_content)
+        
+        return html_parser.get_data()
