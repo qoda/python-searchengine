df48c38c6a478c129d002131db4c8c3e3eca095b
jpbydendyk@gmail.com <info@saidwot.com>
2011-07-27 09:27:00 +0200

    modified to add better profile methods and remove old methods

--- searchengine/profiler.py
+++ searchengine/profiler.py
@@ -5,6 +5,7 @@
 import optparse
 import time
 
+from searchengine.db import MongoDB
 from searchengine.search import SearchIndex
 
 class Profile(object):
@@ -12,43 +13,46 @@ class Profile(object):
     Simple profiler which returns number of indexed urls and average search
     speed.
     """
-    def __init__(self, *args, **kwargs):
-        self.search_index = kwargs.get('search_index', SearchIndex())
-    
     def get_num_items(self):
         """
         Get the number of indexed items.
         """
-        print "Number of indexed items: %s" % self.search_index.index_obj.doc_count()
+        search_index = SearchIndex()
+        print "Number of indexed items: %s" % search_index.index_obj.doc_count()
+        search_index.close()
+    
+    def get_num_indexed_urls(self):
+        """
+        Get the number of indexed urls.
+        """
+        database = MongoDB(collection_name="crawler_urls")
+        print "Number of indexed urls being crawled: %s" % database.collection.find().count()
     
     def get_search_speed(self):
         """
         Get the average speed at which searches are performed over a predefined
         list of random keywords.
         """
+        search_index = SearchIndex()
         result_time = []
         for q in ['dog', 'cat', 'website', 'news', 'social']:
             start_time = time.clock()
-            results = self.search_index.search(q)
+            results = search_index.search(q)
             end_time = time.clock()
             result_time.append(end_time - start_time)
         
+        search_index.close()
         print "Average search speed: %s secs" % (round(sum(result_time) / len(result_time), 2))
-        
-    def close(self):
-        """
-        Close the database connection and search index.
-        """
-        self.search_index.close()
     
 if __name__ == '__main__':
     
     # parse through the system arguments
     usage = "Usage: %prog [options]"
     parser = optparse.OptionParser()
-    parser.add_option("-n", "--num-items", dest="num_items", action="store_true", help="output the number of indexed items")
-    parser.add_option("-s", "--search-speed", dest="search_speed", action="store_true", help="output the average speed at which searches are performed")
-    parser.add_option("-a", "--all", dest="all", action="store_true", default=True, help="output all profiled values")
+    parser.add_option("-n", "--num-items", dest="num_items", action="store_true", default=False, help="output the number of indexed items")
+    parser.add_option("-u", "--num-urls", dest="num_urls", action="store_true", default=False, help="output the number of indexed urls being crawled")
+    parser.add_option("-s", "--search-speed", dest="search_speed", action="store_true", default=False, help="output the average speed at which searches are performed")
+    parser.add_option("-a", "--all", dest="all", action="store_true", default=False, help="output all profiled values")
     options, args = parser.parse_args()
     
     profiler = Profile()
@@ -56,9 +60,9 @@ if __name__ == '__main__':
     # output the profile result depending on the options
     if options.num_items or options.all:
         profiler.get_num_items()
+    
+    if options.num_urls or options.all:
+        profiler.get_num_indexed_urls()
         
     if options.search_speed or options.all:
         profiler.get_search_speed()
-    
-    # close all connections
-    profiler.close()
