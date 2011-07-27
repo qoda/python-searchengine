41c00e3bc0c92ff76854329df4e41c27f535f7b8
jpbydendyk@gmail.com <info@saidwot.com>
2011-07-27 09:29:34 +0200

    added mongo and http settings

--- searchengine/settings.py
+++ searchengine/settings.py
@@ -8,17 +8,18 @@ application.
 import os
 
 # debug mode setting - logs info msgs
-DEBUG = False
+DEBUG = True
 
 # mongodb settings
-DATABASE_PATH = os.path.join(os.path.dirname(__file__), "rsscrawlers.sqlite")
-SYNC_FILE_PATH = ""
+MONGO_NAME = "searchengine"
+MONGO_HOST = "qoda-macbook.local"
+MONGO_PORT = 27017
 
 # url connection settings
-CONNECTION_TIMEOUT = 30
+CONNECTION_TIMEOUT = 15
 
 # whoosh index settings
-INDEX_PATH = "/var/whoosh/rssindex"
+INDEX_PATH = "/tmp/searchengine"
 
 # crawler settings
 CRAWL_DEPTH = 3
@@ -27,9 +28,6 @@ CRAWL_DEPTH = 3
 LOG_PATH = "/tmp/"
 LOG_FORMAT = "%(levelname)s [%(class)s.%(method)s]: %(message)s (%(asctime)-15s)"
 LOG_FILE_NAME = "searchengine.log"
-
-# backup settings
-BACKUP_PATH = "/var/backups/rsscrawler"
 
 # general settings
 MAX_PROCESS_RECURSION = 3
