536a02e7cc094debca5432c58116f12e36c55ea7
Jonathan Bydendyk <jpbydendyk@gmail.com>
2011-08-03 17:21:47 +0200

    added support for multiprocessing

--- searchengine/spider.py
+++ searchengine/spider.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
+from multiprocessing import Pool
 import optparse
 from urlparse import urlparse, urljoin
 
 from searchengine.db import MongoDB
 from searchengine.logger import Logging
 from searchengine.scraper import HTMLScraper
+from searchengine.settings import MAX_SPIDER_PROCESSES
 
 class Spider(object):
     """
@@ -34,56 +36,87 @@ class Spider(object):
         Test if the link is an external link.
         """
         return urlparse(base_url).hostname != urlparse(url).hostname
+
+    def spider(self, obj, link):
+            
+        # ensure the full url is used rather than the relative link
+        if not link.startswith("http"):
+            link = urljoin(obj['url'], link)
+        
+        # check the link has not already been added, else add it
+        if self.is_external_link(obj['url'], link):
+
+            # reform the url with its schema and hostname
+            external_url = "%s://%s" % (urlparse(link).scheme, urlparse(link).hostname)
+            self.log.info("Spider", "run", "Found new external url: %s" % external_url)
+
+            # save the new external url to the database
+            self.database.insert({
+                'url': external_url,
+                'last_crawled': None,
+                'valid': True,
+                'sub_urls': [],
+            })
+            
+            return external_url
+            
+        # if its an external link add it to the database
+        else:
+            if link not in obj['sub_urls']:
+                self.log.info(
+                    "Spider", 
+                    "run", 
+                    "Found new internal url for %s: %s" % (obj['url'], link)
+                )
+
+                obj['sub_urls'].append(link)
+                self.database.update(
+                    query_data={'_id': obj['_id']}, 
+                    data={'sub_urls': obj['sub_urls']}
+                )
+
+        return None
     
     def run(self):
         """
         Get a list of current urls, spider each url independantly to look for
         more urls to add to the database.
         """
-        new_url_count = 0
         obj_list = [obj for obj in self.database.find()]
+
+        # ensure the obj_list isn't empty
         if not obj_list:
             self.log.error("Spider", "run", "No urls found to spider.")
             
         # start spidering the urls
         external_urls = [obj['url'] for obj in obj_list]
         self.log.info("Spider", "run", "Started spidering %s sites for new urls" % len(obj_list))
+
+        # instantiate the argument list to be used when mapping the the process pool
+        args_list = []
+        new_url_count = 0
         for obj in obj_list:
             links = self.get_links(obj['url'])
             for link in links:
-                
-                # ensure the full url is used rather than the relative link
-                if not link.startswith("http"):
-                    link = urljoin(obj['url'], link)
-                
-                # check the link has not already been added, else add it
-                if self.is_external_link(obj['url'], link):
-                    external_url = "%s://%s" % (urlparse(link).scheme, urlparse(link).hostname)
-                    if external_url not in external_urls:
-                        self.log.info("Spider", "run", "Found new external url: %s" % external_url)
-                        new_url_count += 1
-                        self.database.insert({
-                            'url': external_url,
-                            'last_crawled': None,
-                            'valid': True,
-                            'sub_urls': [],
-                        })
-                        external_urls.append(external_url)
-                    
-                # if its an external link add it to the database
-                else:
-                    if link not in obj['sub_urls']:
-                        self.log.info("Spider", "run", "Found new internal url for %s: %s" % (obj['url'], link))
-                        new_url_count += 1
-                        obj['sub_urls'].append(link)
-                        self.database.update(query_data={'_id': obj['_id']}, data={'sub_urls': obj['sub_urls']})
+                args_list.append((obj, link))
+                new_url_count += 1
         
-        self.log.info("Spider", "run", "Spidering %s sites completed. %s new urls found (External: %s | Internal: %s)." % (
-            len(obj_list),
-            new_url_count,
-            len(external_urls),
-            new_url_count - len(external_urls)
-        ))
+        # create process pool with a limited number of given argumants
+        process_pool = Pool(processes=MAX_SPIDER_PROCESSES)
+        process_results = process_pool.map(self.spider, args_list)
+
+        external_urls = [u for u in process_results if not None]
+        
+        self.log.info(
+            "Spider", 
+            "run", 
+            "Spidering %s sites completed. %s new urls found (External: %s | Internal: %s)." % (
+                len(obj_list),
+                new_url_count,
+                len(external_urls),
+                new_url_count - len(external_urls)
+            )
+        )
 
 if __name__ == '__main__':
     
